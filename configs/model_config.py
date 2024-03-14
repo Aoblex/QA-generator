c8882c6b5a24a61f54c26226a4961627d025b61a
Chenrui Wang <ChenruiStats@outlook.com>
2024-03-14 04:11:17 +0000

    Updated model config

--- configs/model_config.py
+++ configs/model_config.py
@@ -1,6 +1,6 @@
 MODEL_CONFIGS = {
     "model": "gpt-3.5-turbo-1106",
-    "temperature": 0.0,
-    "frequency_penalty": 0.00,
-    "presence_penalty": 0.00
+    "temperature": 0.2,
+    "frequency_penalty": 0.05,
+    "presence_penalty": 0.05,
 }
